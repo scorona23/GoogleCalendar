3eb02d8f5c0c4d7b0ee4aeeeb91f153e3c1ab6a8
Stephanie Corona <scorona1000@gmail.com>
2016-04-03 20:01:12 -0400

    Style sheets fixed

---
+++ styles.css
@@ -0,0 +1,138 @@
+#btn-danger{
+    background-color: #e60000;
+    padding:10px;
+    color:white;
+    font-weight:bold;
+}
+
+.glyphicon-collapse-down{
+    padding-left: 30px;
+}
+
+.gly2{
+    padding: 5px;
+}
+
+.gear-drop li{
+    width:50px;
+}
+.create{
+    padding-left:5px;
+    width:500px;
+}
+
+nav{
+    background-color: black;
+}
+
+.nav-pills a{
+    color:gray;
+}
+
+ul.nav > li > a:visited{
+    background-color:black;
+}
+
+ul.nav > li > a:hover {
+    background-color: black;
+    color:white;
+}
+
+.dropdown.open{
+ background-color: black;   
+}
+
+.search{
+    background-color:#ccccb3;
+    padding-top: 10px;
+}
+
+.side-info span{
+    padding-right:15px;
+    padding-bottom:20px;
+}
+
+.search .col-xs-4{
+    padding:10px;
+}
+
+.second_menu{
+    border-top: solid 1px gray;
+    border-bottom: solid 1px gray;
+}
+
+.cal .row{
+    border-bottom: solid 1px gray;
+    padding: 10px;
+    border-left: solid 1px gray;
+}
+
+#text_calendar{
+    font-size: 30px;
+    color: red;
+    padding-left: 25px;
+    padding-top:15px;
+    font-weight: bold;
+}
+
+.second_menu div{
+    padding:5px;
+}
+
+.main_calendar #gmt{
+    height:15px;
+}
+
+.main_calendar .col-xs-4{
+    padding: 0px;
+    height:40px;
+}
+
+.main_calendar .col-xs-3{
+    padding: 0px;
+    height:40px;
+}
+
+.main_calendar .col-xs-6{
+    height:40px;
+}
+.main_calendar{
+    padding: 15px;
+}
+
+.mini_calendar{
+    padding: 10px;
+}
+
+.mini_calendar .btn{
+    padding:0px;
+    margin:0px;
+    background:none;
+}
+
+.input-group-addon{
+    border:0px;
+    background:none;
+    text-align:left;
+}
+
+.well{
+    width: 250px;
+    margin:0px;
+    background:none;
+    border:none;
+    box-shadow:none;
+}
+
+.space{
+    padding-top: 10px;
+}
+
+.label-danger{
+    padding:10px;
+}
+
+.main_calendar .cal-text{
+    text-align: center;
+}
+
